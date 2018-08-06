2c7d601b2b62b47d1ac68e9a8d7dd55e9cb37a38
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-06 23:11:35 +0100

    I added a class of body to the containing div to be able to style the background

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import SignUpPage from "./Components/SignUpPage";
+import CustomNavbar from "./Components/CustomNavbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="body">
+          <CustomNavbar />
+          <Route path="/" component={SignUpPage} exact/>
+          {/* <Route  path="/" component={Sign-In} /> */}
+          {/* <Route path="/" component={Cart} /> */}
+        </div>
+        
+      </BrowserRouter>
     );
   }
 }
