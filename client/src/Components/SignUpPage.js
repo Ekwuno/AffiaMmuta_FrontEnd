075e31b8f862f40dbc6de9d345da5a3f76cbad75
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-06 16:54:42 +0100

    I added basic colors and styles that weren't in the first commit to the Sign up page

---
+++ client/src/Components/SignUpPage.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { Jumbotron, Grid, Row, Col, Image, Button, Form, FormGroup, ControlLabel, FormControl, Checkbox } from "react-bootstrap";
+import './SignUpPage.css';
+import logo from './Assets/AffiaMmutaLogo.png';
+
+ export default class SignUpPage extends Component {
+  render() {
+    return (
+      <div >
+        <Grid>
+            <Jumbotron>
+                 <img src={logo} alt='Logo' height= "auto" width= "200px" />
+                <h3>Create an Account</h3>
+                <Form>
+                    <FormGroup>
+                       
+                        <Col>
+                            <FormControl placeholder= "Username" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col>
+                            <FormControl placeholder="Email" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                      
+                        <Col >
+                            <FormControl placeholder="Password" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col>
+                            <FormControl placeholder="Confirm Password" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col>
+                            <Button className="btn btn-success myName" type="submit">Get Started</Button>
+                        </Col>
+                    </FormGroup>
+                    <p>Already got an account? <Link to="/">Sign In</Link></p>
+                </Form>;
+            </Jumbotron>
+        </Grid>
+      </div>
+    );
+  }
+}
