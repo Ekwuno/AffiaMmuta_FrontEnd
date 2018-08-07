7fb96a71b4e49e319ddc0b2e4dc5e47049da5881
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-07 23:27:31 +0100

    Apparently, the HomeBody component had to be rendered in this component

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Jumbotron, Grid, Row, Col, Image, Button } from "react-bootstrap";
-import "./HomePage.css";
 import Welcome from "../Components/Welcome";
+import HomeBody from "../Components/HomeBody";
 
  export default class HomePage extends Component {
   render() {
     return (
       <div >
            <Welcome />
+           <HomeBody />
       </div>
     );
   }
