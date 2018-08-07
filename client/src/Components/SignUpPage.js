21385fb89e243cf02cabab4c38ee8b1663701af1
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-07 12:32:24 +0100

    Successfully incorporated a post to the back-end using Axios

--- client/src/Components/SignUpPage.js
+++ client/src/Components/SignUpPage.js
@@ -1,54 +1,80 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Jumbotron, Grid, Row, Col, Image, Button, Form, FormGroup, ControlLabel, FormControl, Checkbox } from "react-bootstrap";
+import axios from "axios";
+import { Jumbotron, Col, Button, Form, FormGroup, FormControl, } from "react-bootstrap";
 import './SignUpPage.css';
 import logo from './Assets/Ikenga.png';
 
  export default class SignUpPage extends Component {
-  render() {
-    return (
-      <div >
-        <Grid>
-            <Jumbotron>
-                 <img src={logo} alt='Logo' height= "69px" width= "97px" />
-                <h3>Create an Account</h3>
-                <Form>
-                    <FormGroup>
-                       
-                        <Col>
-                            <FormControl placeholder= "Username" />
-                        </Col>
-                    </FormGroup>
+     state = {
+        username: "",
+        email: "",
+        password: "",
+     }
 
-                    <FormGroup>
-                        <Col>
-                            <FormControl placeholder="Email" />
-                        </Col>
-                    </FormGroup>
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(data)
+        axios
+        .post("https://affiammuta.herokuapp.com/users/create", data)
+        .then(res =>{ 
+             alert(JSON.stringify(res.data.message));
+             console.log(res.data)
+         })
+    }
+    
+    render() {
+        return (
+            <div >
+                <Jumbotron className= "jumbo">
+                    <img src={logo} alt='Logo' height= "69px" width= "97px" />
+                    <h3>Create an Account</h3>
+                    <Form onSubmit= {this.handleSubmit}>
+                        <FormGroup>
+                        
+                            <Col>
+                                <FormControl id="username" placeholder= "Username" onChange= {this.handleChange}  />
+                            </Col>
+                        </FormGroup>
 
-                    <FormGroup>
-                      
-                        <Col >
-                            <FormControl placeholder="Password" />
-                        </Col>
-                    </FormGroup>
+                        <FormGroup>
+                            <Col>
+                                <FormControl id="email" placeholder="Email" onChange= {this.handleChange}/>
+                            </Col>
+                        </FormGroup>
 
-                    <FormGroup>
-                        <Col>
-                            <FormControl placeholder="Confirm Password" />
-                        </Col>
-                    </FormGroup>
+                        <FormGroup>
+                        
+                            <Col >
+                                <FormControl id= "password" type= "password" placeholder="Password" onChange= {this.handleChange}/>
+                            </Col>
+                        </FormGroup>
 
-                    <FormGroup>
-                        <Col>
-                            <Button className="btn btn-success myName" type="submit">Get Started</Button>
-                        </Col>
-                    </FormGroup>
-                    <p>Already got an account? <Link to="/">Sign In</Link></p>
-                </Form>;
-            </Jumbotron>
-        </Grid>
-      </div>
+                        <FormGroup>
+                            <Col>
+                                <FormControl type= "password" placeholder="Confirm Password" onChange= {this.handleChange}/>
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Col>
+                                <Button className="btn btn-success myName" type="submit">Get Started</Button>
+                            </Col>
+                        </FormGroup>
+                        <p>Already got an account? <Link to="/">Sign In</Link></p>
+                    </Form>;
+                </Jumbotron>
+        </div>
     );
   }
 }
