59cc8834719727424b086675deb2a7981b72babe
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-08 12:21:06 +0100

    I succeded in making the Logo show on the header by removing the brand tag

--- client/src/Components/CustomNavbar.js
+++ client/src/Components/CustomNavbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Navbar, Nav, NavItem, Button, Glyphicon } from "react-bootstrap";
-import logo from './Assets/Ikenga.png';
+import { Navbar, Nav, NavItem, Image, Button, Glyphicon } from "react-bootstrap";
+import logo from './Assets/AffiaMmutaLogo.png';
 import './SignUpPage.css';
 import './CustomNavbar.css';
 
@@ -10,29 +10,22 @@ import './CustomNavbar.css';
   render() {
     return (
       <Navbar default collapseOnSelect>
-         <Navbar.Header>
-          <Navbar.Brand>
-              <img src={logo} alt='Logo' height= "auto" width= "80px" />
-              {/* <Image src="/thumbnail.png" responsive />; */}
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-                  <Nav pullRight>
-                    <NavItem eventKey={1}>
-                        <Button className="btn btn-success theButton" type="submit">Market Place</Button>
-                    </NavItem>
-                    <NavItem eventKey={2} className= "login-button">
-                    Login
-                    </NavItem>
-                    {/* <FieldGroup
-                        id="formControlsText"
-                        type="text"
-                        placeholder="Enter text" /> */}
-                        <input type = "Text" placeholder= "Search" className="search-bar" />
-                          <Glyphicon glyph="camera" className="glyph"/>
-                    </Nav>
-         </Navbar.Collapse>
+          <Navbar.Header>
+            <Image src={logo} alt='Logo' className="image" />
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1}>
+                <Button className="btn btn-success theButton" type="submit">Market Place</Button>
+              </NavItem>
+              <NavItem eventKey={2} className= "login-button">
+                Login
+              </NavItem>
+                <input type = "Text" placeholder= "Search" className="search-bar" />
+                <Glyphicon glyph="camera" className="glyph"/>
+            </Nav>
+          </Navbar.Collapse>
       </Navbar>
                
     );
