6aefd3d45d62fa581ad9eea6919b3157221692e6
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-09 09:25:49 +0100

    I updated the search icon

--- client/src/Components/CustomNavbar.js
+++ client/src/Components/CustomNavbar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem, Image, Button, FormGroup, FormControl } from "react-bootstrap";
 import logo from './Assets/AffiaMmutaLogo.png';
 import cart from './Assets/Cart.png';
+import search from './Assets/Search.png';
 import './SignUpPage.css';
 import './CustomNavbar.css';
 
@@ -34,7 +35,7 @@ import './CustomNavbar.css';
                 <FormControl type="text" placeholder="I am looking for..." className="search" />
               </FormGroup>{' '}
               <Button type="submit" className= "button">
-              <i class="fas fa-search"></i>
+                <Image src={search} alt='Logo' className="" />
               </Button>
               <Link to="/Cart">
                 <Button type="submit" className= "cart">
