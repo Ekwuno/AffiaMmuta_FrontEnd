1f2f6a59a88e5a0b4bc14843e20c40a81f3b5d46
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-09 02:54:36 +0100

    I changed the contents of the header to the new contents on the mockup

--- client/src/Components/CustomNavbar.js
+++ client/src/Components/CustomNavbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Navbar, Nav, NavItem, Image, Button, Glyphicon } from "react-bootstrap";
+import { Navbar, Nav, NavItem, Image, Button, FormGroup, FormControl } from "react-bootstrap";
 import logo from './Assets/AffiaMmutaLogo.png';
 import './SignUpPage.css';
 import './CustomNavbar.css';
@@ -15,15 +15,24 @@ import './CustomNavbar.css';
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem eventKey={1}>
-                <Button className="btn btn-success theButton" type="submit">Market Place</Button>
+            <Nav pullRight className="nav" >
+              <NavItem>
+                <Button className="btn btn-success theButton" type="submit">Market</Button>
               </NavItem>
-              <NavItem eventKey={2} className= "login-button">
-                Login
+              <NavItem>
+                <Button className="btn btn-success theButton" id="login-button" type="submit">Login</Button>
               </NavItem>
-                <input type = "Text" placeholder= "Search" className="search-bar" />
-                <Glyphicon glyph="camera" className="glyph"/>
+              <Navbar.Form>
+              <FormGroup>
+                <FormControl type="text" placeholder="I am looking for..." className="search" />
+              </FormGroup>{' '}
+              <Button type="submit" className= "button">
+                <i class="fas fa-search"></i>
+              </Button>
+              <Button type="submit" className= "cart">
+                <i class="fas fa-shopping-cart"></i>
+              </Button>
+              </Navbar.Form>
             </Nav>
           </Navbar.Collapse>
       </Navbar>
