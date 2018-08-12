0848fc0919b34793f33a21be30db779d8850870a
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-12 21:57:41 +0100

    I modified the existing log in page and added the layout on the mock up to it

--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Jumbotron } from "react-bootstrap";
-
+import { Jumbotron, Row, Col, Button, Form, FormGroup, FormControl, } from "react-bootstrap";
+import './LogIn.css';
+import logo from './Assets/Ikenga.png';
 
 export default class LogIn extends Component {
   render() {
     return (
-        <div>
-            <Jumbotron>
-                <h1>Log in page coming soon...</h1>
-            </Jumbotron>
+        <div className="sign-in-background" >
+                <Jumbotron className= "sign-in-box">
+                    <img src={logo} alt='Logo'/>
+                    <h3>Let's setup your account</h3>
+                    <Form>
+                        <FormGroup>
+                            <Row><p>Enter your username</p></Row>
+                            <Row className="sign-in-rows">
+                                <FormControl />
+                            </Row>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Row><p>Enter your password</p></Row>
+                            <Row className="sign-in-rows">
+                                <FormControl type="password" />
+                            </Row>
+                        </FormGroup>
+                        <FormGroup>
+                            <Row>
+                                <Button className="btn btn-success btn-get-started" type="submit">Let's Begin</Button>
+                            </Row>
+                        </FormGroup>
+                    </Form>;
+                </Jumbotron>
         </div>
     );
   }
