03298d8bb86f0f78f7fe5d52b7b19b3ccecfd31d
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-13 09:34:56 +0100

    I added a post request for the username and the password om the log in  page

--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Jumbotron, Row, Col, Button, Form, FormGroup, FormControl, } from "react-bootstrap";
+import axios from "axios";
 import './LogIn.css';
 import logo from './Assets/Ikenga.png';
 
+
 export default class LogIn extends Component {
+    state = {
+        username: "",
+        password: "",
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            password: this.state.password
+        }
+        console.log(data)
+        axios
+        .post("https://affiammuta.herokuapp.com/users/create", data)
+        .then(res =>{ 
+             alert(JSON.stringify(res.data.message));
+             console.log(res.data)
+        })
+    }
+
+    
   render() {
     return (
         <div className="sign-in-background" >
                 <Jumbotron className= "sign-in-box">
                     <img src={logo} alt='Logo'/>
                     <h3>Let's setup your account</h3>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <FormGroup>
                             <Row><p>Enter your username</p></Row>
                             <Row className="sign-in-rows">
-                                <FormControl />
+                                <FormControl type="username" id="logName" onChange={this.handleChange} />
                             </Row>
                         </FormGroup>
 
                         <FormGroup>
                             <Row><p>Enter your password</p></Row>
                             <Row className="sign-in-rows">
-                                <FormControl type="password" />
+                                <FormControl type="password" id="logPassword" onChange={this.handleChange} />
                             </Row>
                         </FormGroup>
                         <FormGroup>
