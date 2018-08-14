06f68563dda7fb6dd5b0fcb39a442ab3b3eba978
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-15 00:04:54 +0100

    I added the side bar component to the body

--- client/src/Components/AdminPage.js
+++ client/src/Components/AdminPage.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, ListGroup, ListGroupItem, Badge } from "react-bootstrap";
 import './AdminPage.css';
+// import AdminFilters from './AdminFIlters';
 
 
 export default class AdminPage extends Component {
   render() {
     return (
-        <div className="admin-height">
-            <Jumbotron>
-                <h1>Admin page coming soon...</h1>
-            </Jumbotron>
+        <div className="admin-main">
+            
+        <div className="admin-body">
+            <div className="right-filter">
+                <ListGroup className="nav-lists">
+                    <ListGroupItem href="#link1" className="list-style" >
+                        <p >All <Badge className="all">50</Badge></p> 
+                    </ListGroupItem>
+                    <ListGroupItem href="#link1" className="list-style">
+                        <p >Educational <Badge className="edu">20</Badge></p> 
+                    </ListGroupItem>
+                    <ListGroupItem href="#link1" className="list-style">
+                        <p >Folklores <Badge className="folk">30</Badge></p> 
+                    </ListGroupItem>
+                    <ListGroupItem href="#link1" className="list-style">
+                        <p >Comics <Badge className="comc">10</Badge></p> 
+                    </ListGroupItem>
+                    </ListGroup>
+            </div>
+            <div className="left-body"></div>
+        </div>
         </div>
     );
   }
