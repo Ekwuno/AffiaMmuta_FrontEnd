1114c951ed0bcee7d64997341f77cc6763497cb6
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-14 22:18:36 +0100

    I deleted unecessary code

--- client/src/Components/AdminNavbar.js
+++ client/src/Components/AdminNavbar.js
@@ -32,48 +32,6 @@ export default class AdminNavbar extends Component {
                 </Navbar>
             </Col>
         </div>
-    //   <Navbar default collapseOnSelect>
-    //     <Navbar.Header>
-    //       <Link to="/">
-    //         <Image src={logo} alt='Logo' className="image" />
-    //       </Link>
-    //       <Navbar.Toggle />
-    //     </Navbar.Header>
-    //       <Navbar.Collapse>
-    //         <Nav pullRight className="nav" >
-    //           <NavItem>
-    //             <Link to="/Market">
-    //             <Button className="btn btn-success buttons buttons-shadow-market" type="submit">
-    //             <Image src={market} alt='Logo' className="market-image" />
-    //               Market
-    //             </Button>
-    //             </Link>
-    //           </NavItem>
-    //           <NavItem>
-    //             <Link to="/Log-in">
-    //               <Button className="btn btn-success buttons buttons-shadow-login" id="login-button" type="submit">
-    //               <Image src={logIn} alt='Logo' className="log-in-image" />
-    //                 Login
-    //               </Button>
-    //             </Link>
-    //           </NavItem>
-    //           <Navbar.Form>
-    //           <FormGroup>
-    //             <FormControl type="text" placeholder="I am looking for..." className="search" />
-    //           </FormGroup>
-    //           <Button type="submit" className= "button">
-    //             <Image src={search} alt='Logo'/>
-    //           </Button>
-    //           <Link to="/Cart">
-    //             <Button type="submit" className= "cart">
-    //               <Image src={cart} alt='Logo' className="cart-img" />
-    //             </Button>
-    //           </Link>
-    //           </Navbar.Form>
-    //         </Nav>
-    //       </Navbar.Collapse>
-    //   </Navbar>
-               
-    );
+   );
   }
 }
