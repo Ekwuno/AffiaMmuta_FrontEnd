585680a3dbc5c204b9e6467a1c263bc09f4a7987
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-14 15:06:20 +0100

    I added an axios get request for facebook log in

--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -21,55 +21,60 @@ export default class LogIn extends Component {
     handleSubmit = event => {
         event.preventDefault();
         const data = {
-            username: this.state.username,
+            email: this.state.email,
             password: this.state.password
         }
         console.log(data)
         axios
-        .post("https://affiammuta.herokuapp.com/users/create", data)
+        .post("https://affiammuta.herokuapp.com/login", data)
         .then(res =>{ 
-             alert(JSON.stringify(res.data.message));
-             console.log(res.data)
+            alert(JSON.stringify(res.data.message));
+            console.log(res.data)
+        })
+    }
+    facebookLogIn = event => {
+        axios.get("https://affiammuta.herokuapp.com/login/facebook")
+        .then(res=>{
+            alert(res)
+
         })
     }
 
-
-  render() {
+render() {
     return (
         <div className="sign-in-background" >
-                <Jumbotron className= "sign-in-box">
-                    <Image src={logo} alt='Logo'/>
-                    <h3>Let's setup your account</h3>
-                    <Form onSubmit={this.handleSubmit}>
-                        <FormGroup>
-                            <Row><p>Enter your username</p></Row>
-                            <Row className="sign-in-rows">
-                                <FormControl type="username" id="logName" onChange={this.handleChange} />
-                            </Row>
-                        </FormGroup>
-
-                        <FormGroup>
-                            <Row><p>Enter your password</p></Row>
-                            <Row className="sign-in-rows">
-                                <FormControl type="password" id="logPassword" onChange={this.handleChange} />
-                            </Row>
-                        </FormGroup>
-                        <FormGroup>
-                            <Row>
-                                <Button className="btn btn-success btn-log-in" type="submit">Let's Begin</Button>
-                            </Row>
-                        </FormGroup>
-                        <FormGroup>
-                            <Row>
-                                <Button className="btn btn-success btn-log-in btn-facebook" type="submit">
-                                    <Image src={facebook} alt='Logo' />
-                                    Login with Facebook
-                                </Button>
-                            </Row>
-                        </FormGroup>
-                        <p id="log-in" >You don't have an account? <Link to="/Sign-up">Sign Up</Link></p>
-                    </Form>;
-                </Jumbotron>
+            <Jumbotron className= "sign-in-box">
+                <Image src={logo} alt='Logo'/>
+                <h3>Let's setup your account</h3>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Row><p>Enter your Email address</p></Row>
+                        <Row className="sign-in-rows">
+                            <FormControl type="email" id="email" onChange={this.handleChange} />
+                        </Row>
+                    </FormGroup>
+                    <FormGroup>
+                        <Row><p>Enter your password</p></Row>
+                        <Row className="sign-in-rows">
+                            <FormControl type="password" id="password" onChange={this.handleChange} />
+                        </Row>
+                    </FormGroup>
+                    <FormGroup>
+                        <Row>
+                            <Button className="btn btn-success btn-log-in" type="submit">Let's Begin</Button>
+                        </Row>
+                    </FormGroup>
+                </Form>
+                    <FormGroup>
+                        <Row>
+                            <Button className="btn btn-success btn-log-in btn-facebook" type="submit" onClick={this.facebookLogIn}>
+                                <Image src={facebook} alt='Logo' />
+                                Login with Facebook
+                            </Button>
+                        </Row>
+                    </FormGroup>
+                    <p id="log-in" >You don't have an account? <Link to="/Sign-up">Sign Up</Link></p>
+            </Jumbotron>
         </div>
     );
   }
