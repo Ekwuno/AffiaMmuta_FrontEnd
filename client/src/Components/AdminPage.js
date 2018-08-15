ccad4f2b6f91b128b52dd2411b37a316b141e428
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-15 12:45:52 +0100

    I added a basic layout to the admin add book page from the mockups

--- client/src/Components/AdminPage.js
+++ client/src/Components/AdminPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Jumbotron, ListGroup, ListGroupItem, Badge } from "react-bootstrap";
+import { Jumbotron, ListGroup, ListGroupItem, Badge, FormGroup, FormControl, Button, Image, Form, Row, DropdownButton, MenuItem, Tabs, Tab } from "react-bootstrap";
+import adminSearch from './Assets/Search.png';
 import './AdminPage.css';
-// import AdminFilters from './AdminFIlters';
 
 
 export default class AdminPage extends Component {
@@ -27,7 +27,85 @@ export default class AdminPage extends Component {
                     </ListGroupItem>
                     </ListGroup>
             </div>
-            <div className="left-body"></div>
+            <div className="left-body">
+                <div>
+                    <Form inline>
+                        <FormGroup>
+                            <FormControl type="text" placeholder="Find a book..." className="search search-admin" />
+                            <Button type="submit" className= "button button-admin">
+                                <Image src={adminSearch} alt='Logo'/>
+                            </Button>
+                        </FormGroup>
+                    </Form>
+                </div>
+                <div className="upload-box">
+                    <div className="up-title">
+                        <h1 className="upload-title">Upload Books</h1>
+                    </div>
+                    <div>
+                        <Form>
+                            <Row className="upload-t"><p>Book File</p></Row>
+                            <Row className="upload-forms">
+                                <FormControl
+                                id = ""
+                                type = "file"
+                                label = ""
+                                className="bt" /
+                                >
+                            </Row>
+                            <Row className="upload-t"><p>Book Cover</p></Row>
+                            <Row className="upload-forms">
+                                <FormControl
+                                id = ""
+                                type = "file"
+                                className="bt"/
+                                >
+                            </Row>
+                            <FormGroup className="style-admin-paragraphs">
+                                <Row>  
+                                    <Row><p className="admin-labels">Title</p></Row>
+                                    <FormControl type="text" id=""  className="admin-title-row"/>
+                                </Row>
+                            </FormGroup>
+                            <Form inline>
+                                <FormGroup>
+                                    <Row>  
+                                        <Row><p className="admin-labels">Price</p></Row>
+                                        <FormControl type="number" id=""  className="admin-price-cat cat-admin"/>
+                                    </Row>
+                                </FormGroup>
+                                <FormGroup  className="cat-margin">
+                                    <Row>  
+                                        <Row><p className="cat-margin-title" >Category</p></Row>
+                                        <DropdownButton 
+                                            bsStyle
+                                            title="Select Category"
+                                            id=""
+                                            className = "admin-price-cat cat-admin"
+                                            >
+                                            <MenuItem eventKey="1">Educational</MenuItem>
+                                            <MenuItem eventKey="2">Comics</MenuItem>
+                                            <MenuItem eventKey="2">Folklors</MenuItem>
+
+                                        </DropdownButton>
+                                    </Row>
+                                </FormGroup>
+                            </Form>
+                            <FormGroup>
+                                <Row>  
+                                    <Row><p className="admin-labels">Description</p></Row>
+                                    <FormControl componentClass="textarea"type="text" id=""  className="admin-desc-row"/>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Button className="btn btn-success btn-ad" type="submit">Upload</Button>
+                                </Row>
+                            </FormGroup>
+                    </Form>
+                    </div>
+                </div>
+            </div>
         </div>
         </div>
     );
