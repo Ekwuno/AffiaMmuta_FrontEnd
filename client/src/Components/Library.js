51cb4525953d487fc82d08fb6db09eb77d0ee3fd
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-17 12:47:34 +0100

    updated the layouts

--- client/src/Components/Library.js
+++ client/src/Components/Library.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Jumbotron } from "react-bootstrap";
 import './Library.css';
+import empty from './Assets/IkengaEmpty.png';
+import filled from './Assets/IkengaFilled.png';
+
 
 
 export default class Library extends Component {
@@ -13,9 +16,22 @@ export default class Library extends Component {
                     <h2>Library</h2>
                 </div>
                 <div className="library-contents">
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="library-book-container">
+                        <div className="library-book-position">
+                            <div className="library-book-image"></div>
+                            <div className="library-book-des">
+                                <p className="library-book-name">Under the Udala Tree</p>
+                                <p className="library-book-author">by Chinelo Okparanta</p>
+                                <div className="ikenga-container">
+                                    <span><img src={filled} className="ikenga-filled"/></span>
+                                    <span><img src={filled} className="ikenga-filled"/></span>
+                                    <span><img src={filled} className="ikenga-filled"/></span>
+                                    <span><img src={filled} className="ikenga-filled"/></span>
+                                    <span><img src={empty} className="ikenga-empty"/></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
