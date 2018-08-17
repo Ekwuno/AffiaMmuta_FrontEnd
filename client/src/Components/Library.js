af18ea7ee929becaf135959f476ffbaa1b7628fb
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-17 22:12:05 +0100

    I completed the design outline using the available assets for the facebook and twitter

--- client/src/Components/Library.js
+++ client/src/Components/Library.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Jumbotron } from "react-bootstrap";
 import './Library.css';
+import image from './Assets/UnderTheUdalaTree.jpg';
 import empty from './Assets/IkengaEmpty.png';
 import filled from './Assets/IkengaFilled.png';
 
@@ -18,7 +19,9 @@ export default class Library extends Component {
                 <div className="library-contents">
                     <div className="library-book-container">
                         <div className="library-book-position">
-                            <div className="library-book-image"></div>
+                            <div className="library-book-image">
+                                <img src={image} className ="lib-cover-img" />
+                            </div>
                             <div className="library-book-des">
                                 <p className="library-book-name">Under the Udala Tree</p>
                                 <p className="library-book-author">by Chinelo Okparanta</p>
@@ -28,6 +31,12 @@ export default class Library extends Component {
                                     <span><img src={filled} className="ikenga-filled"/></span>
                                     <span><img src={filled} className="ikenga-filled"/></span>
                                     <span><img src={empty} className="ikenga-empty"/></span>
+                                </div>
+                                <p className="library-book-date">20/10/2018</p>
+                                <div className="button-connect">
+                                    <div><button className="library-button">Read</button></div>
+                                    <div></div>
+                                    <div></div>
                                 </div>
                             </div>
                         </div>
