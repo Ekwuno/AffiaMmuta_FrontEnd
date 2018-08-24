be5d5ec1af9e2c00b69c6bf68b2f6bc1fdb697fe
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-24 22:09:43 +0100

    Everything in the info main class except the description renders dynamically

--- client/src/Components/BookInfo.js
+++ client/src/Components/BookInfo.js
@@ -1,40 +1,84 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 import { Badge, Tabs, Tab } from "react-bootstrap";
 import './BookInfo.css';
-import udala from './Assets/UnderTheUdalaTree.jpg';
 import filled from './Assets/IkengaFilled.png';
+import axios from 'axios';
 
 
 export default class BookInfo extends Component {
-  render() {
-    return (
+    state = {
+        author: "",
+        title: "",
+        price: "",
+        bookImage: "",
+        ikenga: 0
+    }
+    componentDidMount(){
+        const book = this.props.match.params.id
+        axios.get(`https://affiammuta.herokuapp.com/books/book/${book}`)
+        .then(res =>{
+            this.setState({
+            author: res.data.book.author,
+            title: res.data.book.title,
+            price: res.data.book.price,
+            bookImage: res.data.book.bookImage,
+            description: res.data.book.description,
+            ikenga: res.data.book.ikenga
+            })
+        })
+    }
+
+    payWithPaystack() {
+        const PaystackPop = window.PaystackPop;
+        var handler = PaystackPop.setup({
+            key: 'pk_test_643b16d2cc562281510d64a3afc5a76fbb8dfaa7',
+            email: 'customer@email.com',
+            amount: 10000 * 100,
+            ref: '' + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+            metadata: {
+                custom_fields: [{
+                    display_name: "Mobile Number",
+                    variable_name: "mobile_number",
+                    value: "+2348012345678"
+                }]
+            },
+            callback: function (response) {
+                alert('success. transaction ref is ' + response.reference);
+            },
+            onClose: function () {
+                alert('window closed');
+            }
+        });
+        handler.openIframe();
+    }
+    render() {
+        return (
             <div className="info-main">
                 <div className="current-book">
                     <div className="read-cover">
-                        <img src={udala} alt='Logo' className="read-book-cover"/>
+                        <img src={this.state.bookImage} alt='Logo' className="read-book-cover"/>
                     </div>
                     <div className="read-des">
-                        <h2>Under the Udala Tree</h2>
-                        <p className="rate-title">by Chinelo Okparanta</p>
-                        <p className="rate-price">&#8358;3000</p>
+                        <h2>{this.state.title}</h2>
+                        <p className="rate-title">{this.state.author}</p>
+                        <p className="rate-price">&#8358;{this.state.price}</p>
                         <div className="button-rating">
-                            <button className="buy-book-btn">Buy Now</button>
+                                <script src="https://js.paystack.co/v1/inline.js"></script>
+                                <button type="button" onClick={this.payWithPaystack} className="buy-book-btn"> Buy Now </button> 
                             <div className="book-ratings">
-                                <img src={filled} alt="ratings" className=""/>
-                                <Badge className="badge-rate">6</Badge>
+                                <img src={filled} alt="ratings" />
+                                <Badge className="badge-rate">{this.state.ikenga}</Badge>
                             </div>
                         </div>
-                        <p className="book-des">Inspired by Nigeria's folktales and its war, Under the Udala Trees is a deeply searching, powerful debut about the dangers of living and loving openly.
-                        Ijeoma comes of age as her nation does; born before independence, she is eleven when civil war breaks out in the young republic of Nigeria. Sent away to safety, she meets another displaced child and they, star-crossed, fall ...more</p>
+                        <p className="book-des">{this.state.description}</p>
                     </div>
                 </div>
-                <div>
+                <div className="book-tabs">
                     <Tabs defaultActiveKey={1} >
                         <Tab eventKey={1} title="REVIEWS (3)">
                             Tab 1 content
                         </Tab>
-                        <Tab eventKey={2} title="RELATED BOOKS">
+                        <Tab eventKey={2} title="RELATED BOOKS" className="right-tab">
                             Tab 2 content
                         </Tab>
                     </Tabs>
