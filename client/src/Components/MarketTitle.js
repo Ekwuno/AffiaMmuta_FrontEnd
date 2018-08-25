d1a26158855579c909ed02cf0da76f4876d6e8bf
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-25 11:01:30 +0100

    I created an implemented the basic market title layout design

---
+++ client/src/Components/MarketTitle.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Jumbotron, InputGroup, DropdownButton, MenuItem, Row, Col } from "react-bootstrap";
+import './MarketTitle.css';
+
+
+export default class MarketTitle extends Component {
+  render() {
+    return (
+        <div>
+            <div className="market-title-dropdown">
+                <h1>Market Place</h1>
+                <div>
+                    <InputGroup className="market-btn-group">
+                        <InputGroup.Addon className="market-btn-addon">
+                                    {/* <img src={} alt='Logo' className="form-title-logo"/> */}
+                        </InputGroup.Addon>
+                        <DropdownButton title="select a category">
+                            <MenuItem eventKey="1">Action</MenuItem>
+                            <MenuItem eventKey="2">Another action</MenuItem>
+                            <MenuItem eventKey="3">
+                                Active Item
+                            </MenuItem>
+                        </DropdownButton>
+                    </InputGroup>
+                </div>
+            </div>
+            <Jumbotron className="market-interact-container">
+                <Jumbotron className="market-interact">
+                     <h1>readers today <br />leaders tommorow</h1>
+                </Jumbotron>
+            </Jumbotron>
+        </div>
+    );
+  }
+}
