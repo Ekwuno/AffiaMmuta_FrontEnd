33be7ca0f596f92cf407f62af2ae04a3a60b8ea2
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-25 15:02:46 +0100

    I added an infinite scroll feature to this component

--- client/src/Components/MarketBooks.js
+++ client/src/Components/MarketBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Tabs, Tab, Col, Row, Image, Grid, Button } from "react-bootstrap";
+import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from "axios";
 import cart from './Assets/CustomCart.png';
 import './MarketBooks.css';
@@ -10,7 +11,7 @@ export default class MarketBooks extends Component {
     constructor(){
       super();
       this.state = {
-      allBooks: []
+      allBooks: [],
      }
    }
    componentDidMount(){
@@ -19,6 +20,19 @@ export default class MarketBooks extends Component {
             this.setState({allBooks: res.data});
      })
    }
+   fetchData = () => {
+       // a fake async api call like which sends
+       // 20 more records in 1.5 secs
+    //    setTimeout(() => {
+    //        axios.get("https://affiammuta.herokuapp.com/books/latest")
+    //            .then(res => {
+    //                 this.setState({
+    //                     allBooks: this.state.allBooks.concat(res.data)
+    //                 });
+    //            })
+    //    }, 1500);
+    this.componentDidMount();
+   };
   render() {
         const popular = this.state.allBooks.map(item =>
             <Col xs={12} sm={4} className= "wrapper market-wrapper">
@@ -47,12 +61,26 @@ export default class MarketBooks extends Component {
                 <Tabs defaultActiveKey={1} >
                     <Tab eventKey={1} title="ALL">
                         <Grid className= "grid">
-                            <Row className= "show-grid text-center">
-                            {popular}          
-                            </Row>
+                            <InfiniteScroll dataLength ={this.state.allBooks.length} //This is important field to render the next data
+                                next={this.fetchData}
+                                hasMore={true}
+                                loader={<h4>Loading...</h4>}
+                                endMessage={
+                                    <p style={{textAlign: 'center'}}>
+                                    <b>Yay! You have seen it all</b>
+                                    </p>
+                                }
+                                
+                                releaseToRefreshContent={
+                                    <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
+                                }>
+                                <Row className= "show-grid text-center">
+                                {popular}          
+                                </Row>
+                            </InfiniteScroll>
                         </Grid>
                     </Tab>
-                    <Tab eventKey={2} title="POPULAR BOOKS" className="right-tab">
+                    <Tab eventKey={2}title="POPULAR BOOKS" className="right-tab">
                         Tab 2 content
                     </Tab>
                     <Tab eventKey={3} title="MOST READ BOOKS" className="right-tab">
