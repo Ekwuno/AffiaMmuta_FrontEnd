96647ca20f215fd47f2658fb8827bb500796cf42
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-26 17:57:40 +0100

    Corrected a typographical error

--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -9,7 +9,7 @@ import "./Welcome.css";
       <div >
             <Jumbotron reponsive className= "affia-header">
                 <h3>Welcome to AffiaMmuta</h3>
-                <p>We make learning fun an interactive</p>
+                <p>We make learning fun and interactive</p>
                 <Link to="/Sign-up">
                   <Button className="btn btn-success get-btn" type="submit">
                       Get Started
