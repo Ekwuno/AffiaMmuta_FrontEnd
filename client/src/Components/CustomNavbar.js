f5628116ed8ff78504553ad9b6f5e9248528e3eb
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-26 22:22:48 +0100

    Created two instances of the custom navbar

--- client/src/Components/CustomNavbar.js
+++ client/src/Components/CustomNavbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Navbar, Nav, NavItem, Image, Button, FormGroup, FormControl } from "react-bootstrap";
+import { Navbar, Nav, NavItem, Image, Button, FormGroup, FormControl, DropdownButton, MenuItem } from "react-bootstrap";
 import logo from './Assets/AffiaMmutaLogo.png';
 import cart from './Assets/Cart.png';
 import search from './Assets/Search.png';
@@ -13,6 +13,49 @@ import './CustomNavbar.css';
  class CustomNavbar extends Component {
   render() {
     return (
+      // <Navbar default collapseOnSelect className="general-navbar">
+      //   <Navbar.Header>
+      //     <Link to="/">
+      //       <Image src={logo} alt='Logo' className="image" />
+      //     </Link>
+      //     <Navbar.Toggle />
+      //   </Navbar.Header>
+      //     <Navbar.Collapse>
+      //       <Nav pullRight className="general-navbar-nav" >
+      //         <NavItem>
+      //           <Link to="/Market">
+      //           <Button className="btn btn-success buttons buttons-shadow-market" type="submit">
+      //           <Image src={market} alt='Logo' className="market-image" />
+      //             Market
+      //           </Button>
+      //           </Link>
+      //         </NavItem>
+      //         <NavItem>
+      //           <Link to="/Log-in">
+      //             <Button className="btn btn-success buttons buttons-shadow-login" id="login-button" type="submit">
+      //             <Image src={logIn} alt='Logo' className="log-in-image" />
+      //               Login
+      //             </Button>
+      //           </Link>
+      //         </NavItem>
+      //         <Navbar.Form>
+      //         <FormGroup>
+      //           <FormControl type="text" placeholder="I am looking for..." className="search" />
+      //         <Button type="submit" className= "button">
+      //           <Image src={search} alt='Logo'/>
+      //         </Button>
+      //         </FormGroup>
+      //         <Link to="/Cart">
+      //           <Button type="submit" className= "cart">
+      //             <Image src={cart} alt='Logo' className="cart-img" />
+      //           </Button>
+      //         </Link>
+      //         </Navbar.Form>
+      //       </Nav>
+      //     </Navbar.Collapse>
+      // </Navbar>
+      
+      
       <Navbar default collapseOnSelect className="general-navbar">
         <Navbar.Header>
           <Link to="/">
@@ -34,7 +77,7 @@ import './CustomNavbar.css';
                 <Link to="/Log-in">
                   <Button className="btn btn-success buttons buttons-shadow-login" id="login-button" type="submit">
                   <Image src={logIn} alt='Logo' className="log-in-image" />
-                    Login
+                    Library
                   </Button>
                 </Link>
               </NavItem>
@@ -45,16 +88,18 @@ import './CustomNavbar.css';
                 <Image src={search} alt='Logo'/>
               </Button>
               </FormGroup>
-              <Link to="/Cart">
-                <Button type="submit" className= "cart">
-                  <Image src={cart} alt='Logo' className="cart-img" />
-                </Button>
-              </Link>
+              <DropdownButton 
+                bsStyle
+                title={<Image src={logIn} alt='Logo'/>}
+                id = "dropdown-user"
+                noCaret
+                >
+                  <MenuItem eventKey="1">Log out</MenuItem>
+                </DropdownButton>
               </Navbar.Form>
             </Nav>
           </Navbar.Collapse>
-      </Navbar>
-               
+      </Navbar>        
     );
   }
 }
