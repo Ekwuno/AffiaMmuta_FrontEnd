b59c13621ad201a305ca25c541e411e2ae9e7711
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-27 22:03:49 +0100

    Redesigned the footer using html grid system

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Jumbotron, Col, Row, Grid, Image } from "react-bootstrap";
+import { Jumbotron, Image } from "react-bootstrap";
 import lfooter from './Assets/AffiaMmutaWhite.png';
 import lfooter2 from './Assets/FooterIcon.png';
 import fbIcon from './Assets/TwitterLogo.png';
@@ -12,39 +12,37 @@ import './Footer.css';
   render() {
     return (
         <Jumbotron className="footer">
-            <Grid className="footer-grid">
-                <Row className="footer-top" >
-                    <Col lg={4} className="footer-contents" >
-                        <Image src={lfooter} alt='Logo' className="footer-logo" />
-                    </Col>
-                    <Col lg={2} className="footer-contents">
-                        <h5>ABOUT AFFIAMMUTA</h5>
-                        <p>We are commited to massively <br /> increasing Igbo literacy in African <br />diaspora using technology.</p>
-                    </Col>
-                    <Col lg={2} className="footer-contents">
-                        <h5>CONTACT</h5>
-                        <Link to="#">Contact@affiammuta.com</Link>
-                        <p>+23490260717</p>
-                    </Col>
-                    <Col lg={2} className="footer-contents">
-                        <h5>NAVIGATION</h5>
-                        <Link to="#">Terms of use</Link><br />
-                        <Link to="#">Privacy policy</Link>
-                    </Col>
-                    <Col lg={2} className="footer-contents">
-                        <Image src={lfooter2} alt='Logo' className="footer-icon" />
-                    </Col>
-                </Row>
-                <Row className="footer-bottom">
-                    <Col lg={6} md={6} >
-                        <p>&copy; 2018 AFFIAMMUTA. All Rights Reserved</p>
-                    </Col>
-                    <Col lg={6} md={6} className="fb-twitter">
-                        <Image src={fbIcon} alt='Logo' className="footer-icon" />
-                        <Image src={tIcon} alt='Logo' className="footer-icon" />
-                    </Col>
-                </Row>
-            </Grid>    
+            <div className="footer-top-contents">
+                <div>
+                    <Image src={lfooter} alt='Logo' className="footer-logo" />
+                </div>
+                <div className="about-footer">
+                    <h5 >ABOUT AFFIAMMUTA</h5>
+                    <p>We are commited to massively <br /> increasing Igbo literacy in African diaspora using technology. Learn more</p>
+                </div>
+                <div  className="contact-footer">
+                    <h5 className="contact-head">CONTACT</h5>
+                    <Link to="#">Contact@affiammuta.com</Link>
+                    <p className="contact-no">+23490260717</p>
+                </div>
+                <div className="nav-footer">
+                    <h5>NAVIGATION</h5>
+                    <Link to="#">Terms of use</Link><br />
+                    <Link to="#" className="contact-no">Privacy policy</Link>
+                </div>
+                <div>
+                    <Image src={lfooter2} alt='Logo' className="footer-icon" />
+                </div>
+            </div>
+            <div className="footer-bottom-contents">
+                <div className="footer-bottom-copy">
+                    <p>&copy; 2018 AFFIAMMUTA. All Rights Reserved</p>
+                </div>
+                <div className="fb-twitter">
+                    <Image src={fbIcon} alt='Logo' className="footer-icon" />
+                    <Image src={tIcon} alt='Logo' className="footer-icon" />
+                </div>
+            </div>
         </Jumbotron>   
     );
   }
