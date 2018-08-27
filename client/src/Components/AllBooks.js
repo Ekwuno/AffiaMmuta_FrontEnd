18e31183c02e72223ac1f51f8f7374cb06d452ee
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-27 16:00:25 +0100

    I removed the infinite scroll function

--- client/src/Components/AllBooks.js
+++ client/src/Components/AllBooks.js
@@ -6,31 +6,17 @@ import cart from './Assets/CustomCart.png';
 
 
 export default class AllBooks extends Component {
-        constructor(){
-            super();
-            this.state = {
-            allBooks: [],
-            }
-        }
+          state = {
+              allBooks: [],
+          };
+
         componentDidMount(){
                 axios.get("https://affiammuta.herokuapp.com/books/latest")
                 .then(res=>{
                     this.setState({allBooks: res.data});
             })
         }
-        fetchData = () => {
-            // a fake async api call like which sends
-            // 20 more records in 1.5 secs
-            //    setTimeout(() => {
-            //        axios.get("https://affiammuta.herokuapp.com/books/latest")
-            //            .then(res => {
-            //                 this.setState({
-            //                     allBooks: this.state.allBooks.concat(res.data)
-            //                 });
-            //            })
-            //    }, 1500);
-            this.componentDidMount();
-        };
+        
     render() {
         const allbooks = this.state.allBooks.map(item =>
             <Col xs={12} sm={4} className= "wrapper market-wrapper">
@@ -47,7 +33,7 @@ export default class AllBooks extends Component {
                     </Col>
                     <Col className="book-price" >
                     <Button className="book-buttons" type="submit">
-                        <Image src={cart} alt='cart' className="cart-books" />
+                        <p>Buy</p>
                     </Button>
                     </Col>
                 </Row>
@@ -56,23 +42,9 @@ export default class AllBooks extends Component {
     return (
         <div>
             <Grid className= "grid">
-                <InfiniteScroll dataLength ={this.state.allBooks.length} //This is important field to render the next data
-                next={this.fetchData}
-                hasMore={true}
-                loader={<h4>Loading...</h4>}
-                endMessage={
-                <p style={{textAlign: 'center'}}>
-                <b>Yay! You have seen it all</b>
-                </p>
-                }
-                                
-                releaseToRefreshContent={
-                <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
-                }>
                     <Row className= "show-grid text-center">
                         {allbooks}          
                     </Row>
-                </InfiniteScroll>
             </Grid>
         </div>
     );
