15d4815d25845ca0d0c5e16c88c0950002fe1af1
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-27 13:36:13 +0100

    The axios post now adds books

--- client/src/Components/AdminPage.js
+++ client/src/Components/AdminPage.js
@@ -1,79 +1,75 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import { ListGroup, ListGroupItem, Badge, FormGroup, FormControl, Button, Image, Form, Row, ProgressBar } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
+import { ListGroup, ListGroupItem, Badge, FormGroup, FormControl, Button, Image, Form, Row, ProgressBar, MenuItem, DropdownButton } from "react-bootstrap";
 import adminSearch from './Assets/Search.png';
 import './AdminPage.css';
 
 
-export default class AdminPage extends Component {
+class AdminPage extends Component {
 
     state = {
         bookImage: "",
         bookContent: "",
-        author: "",
-        title: "",
-        price: "",
         progress: 0
     }
     onUploadProgress=() => {
         
     }
-    onChange = (event) => {
+    handleFile = (event) => {
         switch (event.target.name) {
-            case 'selectedFile':
+            case 'bookImage':
                 this.setState({
-                    selectedFile: event.target.files[0]
+                    bookImage: event.target.files[0]
                 })
-                break;
+            case 'bookContent':
+                this.setState({
+                    bookContent: event.target.files[0]
+                })
             default:
-                this.setState({
-                    [event.target.name]: event.target.value
-                })
+                null;
         }
-
+        // console.log(this.state)
     }
     handleSubmit = event  => {
-        event.preventDefault();
-        const {
-            bookImage,
-            bookContent,
-            author,
-            title,
-            price,
-            // bookCategory,
-            // bookDescription
-        } = this.state
+        let author = this.author.value;
+        let title = this.title.value;  
+        let price = this.price.value;  
+        let category = this.category.value;        
+        let description = this.description.value;
+        event.preventDefault()
         const files = new FormData()
-        files.append("bookImage", bookImage);
-        files.append("bookContent", bookContent);
+        files.append("bookImage", this.state.bookImage);
+        files.append("bookContent", this.state.bookContent);
         files.append("author", author);
         files.append("title", title);
         files.append("price", price);
-        // files.append("bookCategory", bookCategory);
-        // files.append("bookDescription", bookDescription);
-        console.log(files)
+        files.append("category", category);
+        files.append("description", description);
+        console.log(files);
 
         axios({
                 method: 'post',
                 url: 'https://affiammuta.herokuapp.com/books/addbook',
                 data: files,
-                 
-                config: {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    }
-                },
-                onUploadProgress: progressEvent => {
-                    let percentCompleted = Math.round((ProgressEvent.loaded *100) / ProgressEvent.total);
-                    console.log(percentCompleted);
-                    console.log(1)
-                    this.setState({progress: percentCompleted})
+                headers: {
+                    'Content-Type': 'mulTipart/form-data'
                 }
+                // onUploadProgress: progressEvent => {
+                //     let percentCompleted = Math.round((ProgressEvent.loaded *100) / ProgressEvent.total);
+                //     console.log(percentCompleted);
+                //     console.log(1)
+                //     this.setState({progress: percentCompleted})
+                // }
 
             })
             .then(res => {
-                console.log(res)
-            })
+                if (res.data.message == "book created successfully") {
+                    this.props.history.push("/")
+                } else {
+                    alert(JSON.stringify(res.data.message));
+                }
+            });
 
     }
 
@@ -96,7 +92,7 @@ export default class AdminPage extends Component {
                         <ListGroupItem href="#link1" className="list-style">
                             <p >Comics <Badge className="comc">10</Badge></p> 
                         </ListGroupItem>
-                        </ListGroup>
+                    </ListGroup>
                 </div>
                 <div className="left-body">
                     <div>
@@ -114,7 +110,7 @@ export default class AdminPage extends Component {
                             <h1 className="upload-title">Upload Books</h1>
                         </div>
                         <div>
-                            <Form action='https://affiammuta.herokuapp.com/books/addbook' method="post" enctype='mulTipart/form-data' onSubmit = {this.handleSubmit}>
+                            <Form encType='multipart/form-data' onSubmit = {this.handleSubmit}>
                                 <Row className="upload-t"><p>Book Cover</p></Row>
                                 <Row className="upload-forms">
                                     <FormControl
@@ -122,7 +118,7 @@ export default class AdminPage extends Component {
                                     type = "file"
                                     label = ""
                                     className="" 
-                                    onChange= {this.handleChange}/
+                                    onChange= {this.handleFile}/
                                     >
                                 </Row>
                                 <Row className="upload-t"><p>Book File</p></Row>
@@ -131,32 +127,34 @@ export default class AdminPage extends Component {
                                     name = "bookContent"
                                     type = "file"
                                     className=""
-                                    onChange= {this.handleChange}
-                                    onClick={this.state.progress}/
+                                    onChange= {this.handleFile}
+                                    /
                                     >
                                 </Row>
                                 <FormGroup className="style-admin-paragraphs">
                                     <Row>  
                                         <Row><p className="admin-labels">Author</p></Row>
-                                        <FormControl type="text" name="author"  className="admin-title-row" onChange= {this.handleChange}/>
+                                        <FormControl inputRef={(ref) => {this.author = ref}} type="text" name="author"  className="admin-title-row" onChange= {this.handleChange}/>
                                     </Row>
                                 </FormGroup>
                                 <FormGroup className="style-admin-paragraphs">
                                     <Row>  
                                         <Row><p className="admin-labels">Title</p></Row>
-                                        <FormControl type="text" name="title"  className="admin-title-row" onChange= {this.handleChange}/>
+                                        <FormControl inputRef={(ref) => {this.title = ref}} type="text" name="title"  className="admin-title-row" onChange= {this.handleChange}/>
                                     </Row>
                                 </FormGroup>
                                     <FormGroup>
                                         <Row>  
                                             <Row><p className="admin-labels">Price</p></Row>
-                                            <FormControl type="number" name="price" step="100" min="100" className="admin-price-cat cat-admin" onChange= {this.handleChange}/>
+                                            <FormControl inputRef={(ref) => {this.price = ref}} type="number" name="price" step="100" min="100" className="admin-price-cat cat-admin" onChange= {this.handleChange}/>
                                         </Row>
                                     </FormGroup>
                                     {/* <FormGroup  className="cat-margin"> */}
-                                        {/* <Row>   */}
-                                            {/* <Row><p className="cat-margin-title" >Category</p></Row> */}
-                                            {/* <DropdownButton 
+                                        <Row> 
+                                            <Row><p className="admin-labels">Category</p></Row>
+                                            <FormControl inputRef={(ref) => {this.category = ref}} type="text" name="category" className="admin-price-cat cat-admin" onChange= {this.handleChange}/> 
+                                            {/* <Row><p className="cat-margin-title" >Category</p></Row>
+                                            <DropdownButton 
                                                 bsStyle
                                                 title="Select Category"
                                                 name="bookCategory"
@@ -165,21 +163,21 @@ export default class AdminPage extends Component {
                                                 >
                                                 <MenuItem eventKey="1">Educational</MenuItem>
                                                 <MenuItem eventKey="2">Comics</MenuItem>
-                                                <MenuItem eventKey="2">Folklors</MenuItem>
+                                                <MenuItem eventKey="3">Folklors</MenuItem>
 
                                             </DropdownButton> */}
-                                        {/* </Row> */}
+                                        </Row>
                                     {/* </FormGroup> */}
                                 {/* <FormGroup> */}
                                     {/* <Row>   */}
-                                        {/* <Row><p className="admin-labels">Description</p></Row> */}
-                                        {/* <FormControl componentClass="textarea"type="text" id="bookDescription"  className="admin-desc-row" onChange= {this.handleChange}/> */}
+                                        <Row><p className="admin-labels">Description</p></Row>
+                                        <FormControl inputRef={(ref) => {this.description = ref}} componentClass="textarea"type="text" name="description"  className="admin-desc-row" onChange= {this.handleChange}/>
                                     {/* </Row> */}
                                 {/* </FormGroup> */}
                                 <FormGroup>
                                     <Row>
-                                        <Button className="btn btn-success btn-ad" type="submit">Upload</Button>
-                                        <ProgressBar  now={this.onUploadProgress} />
+                                        <Button  className="btn btn-success btn-ad" type="submit">Upload</Button>
+                                        {/* <ProgressBar  now={this.onUploadProgress} /> */}
                                     </Row>
                                 </FormGroup>
                         </Form>
@@ -191,3 +189,4 @@ export default class AdminPage extends Component {
         );
     }
 }
+export default withRouter(AdminPage);
