473b4dd4bc63f52276f9c68d43cb1e32800a0423
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-28 23:37:48 +0100

    Designed the search bar for one of the tabs and added a link to the upload books

--- client/src/Components/AdminHome.js
+++ client/src/Components/AdminHome.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Badge } from "react-bootstrap";
+import { Tabs, Tab, Image, FormGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import './AdminHome.css';
 import AdminFilters from './AdminFilters';
+import AdminSearch from './AdminSearch';
+import addbook from './Assets/AddBook.png';
 
  export default class AdminHome extends Component {
   render() {
@@ -10,7 +13,32 @@ import AdminFilters from './AdminFilters';
           <div className="adminhome-left right-filter">
             <AdminFilters/>
           </div>
-          <div className="adminhome-right"></div>
+          <div className="adminhome-right">
+            <div className="admin-home-tabs-position">
+              <Tabs defaultActiveKey={2} >
+                <Tab eventKey={1} title="Videos">
+                  Videos
+                </Tab>
+                <Tab eventKey={2}title="Books">
+                  <div className="admin-search-add">
+                    <div className="admin-home-search">
+                      <AdminSearch/>
+                    </div>
+                    <div className="admin-add">
+                        <span>Add book</span>
+                        <Link to="/admin">
+                          <Image src={addbook} alt=""/>
+                        </Link>
+                    </div>
+                  </div>
+                  
+                </Tab>
+                <Tab eventKey={3} title="Comics">
+                  Comics
+                </Tab>
+              </Tabs>
+            </div>
+          </div>
       </div>
     );
   }
