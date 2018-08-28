9d8ab47843f141e3c6e7d1522d923a0daa95c66c
Bukkysos <chukwuebukaanyanwusos@gmail.com>
2018-08-28 09:42:54 +0100

    Header and footer has to be responsive. So I had to do it

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -13,29 +13,31 @@ import './Footer.css';
     return (
         <Jumbotron className="footer">
             <Grid className="footer-grid">
-                <Row className="footer-top" >
-                    <Col lg={4} className="footer-contents" >
+                <Row className="footer-top footer-rows" >
+                    <Col sm={4} className="footer-contents" >
                         <Image src={lfooter} alt='Logo' className="footer-logo" />
                     </Col>
-                    <Col lg={2} className="footer-contents">
+                    <Col sm={2} className="footer-contents">
                         <h5>ABOUT AFFIAMMUTA</h5>
                         <p>We are commited to massively <br /> increasing Igbo literacy in African <br />diaspora using technology.</p>
                     </Col>
-                    <Col lg={2} className="footer-contents">
+                    <div className="footer-middle">
+                    <Col sm={2} className="footer-contents">
                         <h5>CONTACT</h5>
                         <Link to="#">Contact@affiammuta.com</Link>
                         <p>+23490260717</p>
                     </Col>
-                    <Col lg={2} className="footer-contents">
+                    <Col sm={2} className="footer-contents">
                         <h5>NAVIGATION</h5>
                         <Link to="#">Terms of use</Link><br />
                         <Link to="#">Privacy policy</Link>
                     </Col>
+                    </div>
                     <Col lg={2} className="footer-contents">
                         <Image src={lfooter2} alt='Logo' className="footer-icon" />
                     </Col>
                 </Row>
-                <Row className="footer-bottom">
+                <Row className="footer-bottom footer-rows">
                     <Col lg={6} md={6} >
                         <p>&copy; 2018 AFFIAMMUTA. All Rights Reserved</p>
                     </Col>
