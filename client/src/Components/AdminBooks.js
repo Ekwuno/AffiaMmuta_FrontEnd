c0764dc4ecf6ab729af7ab3b5c1b4d8b9889d77c
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-29 01:21:17 +0100

    I finished up with the layout of the admin home page

--- client/src/Components/AdminBooks.js
+++ client/src/Components/AdminBooks.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { Image, Grid } from "react-bootstrap";
+import { Image, Grid, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import './AdminHome.css';
+import './AdminBooks.css';
 import AdminSearch from './AdminSearch';
 import addbook from './Assets/AddBook.png';
+import phoenix from './Assets/BookOfPhoenix.jpg';
+import remove from './Assets/Delete.png';
 
  export default class AdminBooks extends Component {
   render() {
@@ -20,12 +23,80 @@ import addbook from './Assets/AddBook.png';
                 </Link>
             </div>
         </div>
-        <Grid>
-            <Row>
-                <Col>
-                    
+        <Grid className="adminbooks-grid">
+            <Row className="adminbooks-row">
+                <Col xs={12} sm={4} className="adminbook-container">
+                    <div className="adminbook-img-cont">
+                        <Image src={phoenix} alt='Logo' />
+                    </div>
+                    <div className="adminbook-bottom-contents">
+                        <div>
+                            <h5>Igbo Idioms</h5>
+                            <p>by Mark Uzomba</p>
+                        </div>
+                        <div className="admin-delete">
+                            <Image src={remove} alt='Logo' />
+                        </div>
+                    </div>
+                </Col>
+                <Col xs={12} sm={4} className="adminbook-container">
+                    <div className="adminbook-img-cont">
+                        <Image src={phoenix} alt='Logo' />
+                    </div>
+                    <div className="adminbook-bottom-contents">
+                        <div>
+                            <h5>Igbo Idioms</h5>
+                            <p>by Mark Uzomba</p>
+                        </div>
+                        <div className="admin-delete">
+                            <Image src={remove} alt='Logo' />
+                        </div>
+                    </div>
+                </Col>
+                <Col xs={12} sm={4} className="adminbook-container">
+                    <div className="adminbook-img-cont">
+                        <Image src={phoenix} alt='Logo' />
+                    </div>
+                    <div className="adminbook-bottom-contents">
+                        <div>
+                            <h5>Igbo Idioms</h5>
+                            <p>by Mark Uzomba</p>
+                        </div>
+                        <div className="admin-delete">
+                            <Image src={remove} alt='Logo' />
+                        </div>
+                    </div>
+                </Col>
+                <Col xs={12} sm={4} className="adminbook-container">
+                    <div className="adminbook-img-cont">
+                        <Image src={phoenix} alt='Logo' />
+                    </div>
+                    <div className="adminbook-bottom-contents">
+                        <div>
+                            <h5>Igbo Idioms</h5>
+                            <p>by Mark Uzomba</p>
+                        </div>
+                        <div className="admin-delete">
+                            <Image src={remove} alt='Logo' />
+                        </div>
+                    </div>
                 </Col>
             </Row>
+            <Row className="adminbooks-row">
+                <Col xs={12} sm={4} className="adminbook-container">
+
+                </Col>
+                <Col xs={12} sm={4} className="adminbook-container">
+
+                </Col>
+                <Col xs={12} sm={4} className="adminbook-container">
+
+                </Col>
+                <Col xs={12} sm={4} className="adminbook-container">
+
+                </Col>
+            </Row>
+            
         </Grid>
       </div>
     );
