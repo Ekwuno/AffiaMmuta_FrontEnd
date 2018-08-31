1f6dba4e2cb40396bca87516378e820c0e745629
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-31 15:50:37 +0100

    Rendered the ccomments here dynamically

--- client/src/Components/BookComments.js
+++ client/src/Components/BookComments.js
@@ -7,20 +7,18 @@ import axios from "axios";
      comment: []
    }
    componentDidMount() {
-     axios.get("")
+    //  const bookId = this.props.match.params.id
+     axios.get(`https://affiammuta.herokuapp.com/books/book/5b86a5bf37d63b001fd5e3c8/comments`)
      .then(res => {
        this.setState({comment: res.data})
+        console.log(res.data)
      })
    }
   render() {
     const comment = this.state.comment.map(item=>
       <div className="comment-box">
-        <h2>ibesoft11@gmail.com</h2>
-        <p>I was inspired to pick this up by the blurb claiming that it was 
-            "inspired by Nigeria's folktales." Well, that's not quite true. Certainly
-            , the characters are all Nigerian, and there are a few traditional tales told, 
-            over the course of the book - but the story itself is clearly based on true events, 
-            not on folktales.</p>
+        <h2>{item.user.username}</h2>
+        <p>{item.commentBody}</p>
       </div>
     )
     return (
