058363586158b228afb0bd6b06ffb97b16043995
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-08-31 13:21:59 +0100

    I wrote an axios  post to collect comments from the data base

--- client/src/Components/BookComments.js
+++ client/src/Components/BookComments.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './BookComments.css';
+import axios from "axios";
 
  export default class BookComments extends Component {
+   state = {
+     comment: []
+   }
+   componentDidMount() {
+     axios.get("")
+     .then(res => {
+       this.setState({comment: res.data})
+     })
+   }
   render() {
+    const comment = this.state.comment.map(item=>
+      <div className="comment-box">
+        <h2>ibesoft11@gmail.com</h2>
+        <p>I was inspired to pick this up by the blurb claiming that it was 
+            "inspired by Nigeria's folktales." Well, that's not quite true. Certainly
+            , the characters are all Nigerian, and there are a few traditional tales told, 
+            over the course of the book - but the story itself is clearly based on true events, 
+            not on folktales.</p>
+      </div>
+    )
     return (
       <div>
-        <div className="comment-box">
+        {comment}
+        {/* <div className="comment-box">
             <h2>ibesoft11@gmail.com</h2>
             <p>I was inspired to pick this up by the blurb claiming that it was 
                 "inspired by Nigeria's folktales." Well, that's not quite true. Certainly
@@ -20,7 +41,7 @@ import './BookComments.css';
                 , the characters are all Nigerian, and there are a few traditional tales told, 
                 over the course of the book - but the story itself is clearly based on true events, 
                 not on folktales.</p>
-        </div>
+        </div> */}
       </div>
     );
   }
