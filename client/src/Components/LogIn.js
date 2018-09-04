19a0aded548e12a20116ca0f20d882990db9956a
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2018-09-04 16:58:07 +0100

    I added loaders to the sign up and log in page

--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -7,12 +7,14 @@ import logo from './Assets/Ikenga.png';
 import facebook from './Assets/FacebookLogo.png';
 import email from './Assets/EmailLogo.png';
 import lock from './Assets/Padlock.png';
+import Loader from './Loader';
 
 
 export default class LogIn extends Component {
     state = {
         username: "",
         password: "",
+        isSigningIn: false
     }
 
     handleChange = event => {
@@ -21,6 +23,7 @@ export default class LogIn extends Component {
         });
     }
     handleSubmit = event => {
+        this.setState({isSigningIn: true})
         event.preventDefault();
         const data = {
             email: this.state.email,
@@ -54,7 +57,56 @@ export default class LogIn extends Component {
     }
 
 render() {
-    return (
+    if (this.state.isSigningIn == true ) {
+        return (
+             <div className="sign-in-background" >
+            <Jumbotron className= "sign-in-box">
+                <Image src={logo} alt='Logo'/>
+                <h3>Welcome back!</h3>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup className="style-formgroups">
+                        <Row><p>Enter your Email address</p></Row>
+                        <Row className="sign-in-rows">
+                            <InputGroup className="login-input-group">
+                                    <InputGroup.Addon>
+                                        <img src={email} alt='Logo' className="form-title-logo"/>
+                                    </InputGroup.Addon>
+                                    <FormControl type="email" id="email" onChange={this.handleChange} />
+                                </InputGroup>
+                        </Row>
+                    </FormGroup>
+                    <FormGroup className="style-formgroups">
+                        <Row><p>Enter your password</p></Row>
+                        <Row className="sign-in-rows">
+                                <InputGroup className="login-input-group">
+                                    <InputGroup.Addon>
+                                        <img src={lock} alt='Logo' className="form-title-logo"/>
+                                    </InputGroup.Addon>
+                                    <FormControl type="password" id="password" onChange={this.handleChange} />
+                                </InputGroup>
+                        </Row>
+                    </FormGroup>
+                    <FormGroup className="style-formgroups">
+                        <Row>
+                            <Button className="btn btn-success btn-log-in" type="submit">Let's Begin <Loader size ="small"/></Button>
+                        </Row>
+                    </FormGroup>
+                </Form>
+                    <FormGroup className="style-formgroups">
+                        <Row>
+                            <Button className="btn btn-success btn-log-in btn-facebook" type="submit" onClick={this.facebookLogIn}>
+                                <Image src={facebook} alt='Logo' />
+                                Login with Facebook
+                            </Button>
+                        </Row>
+                    </FormGroup>
+                    <p id="log-in" >You don't have an account? <Link to="/signup">Sign Up</Link></p>
+            </Jumbotron>
+        </div>
+        )
+    }
+    else {
+        return (
         <div className="sign-in-background" >
             <Jumbotron className= "sign-in-box">
                 <Image src={logo} alt='Logo'/>
@@ -100,5 +152,6 @@ render() {
             </Jumbotron>
         </div>
     )
+    }
   }
 }
